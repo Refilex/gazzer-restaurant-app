280dcdc9781eaa4410fc0a50660474eca7421817
ahmedkamal22 <ak0850000@gmail.com>
2024-07-14 13:38:55 +0300

    In this branch I changed the base url to refilex

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -5,7 +5,7 @@ class AppConstants {
   static const String appName = 'Gazzer Restaurant';
   static const double appVersion = 7.6;
 
-  static const String baseUrl = 'https://stackfood-admin.6amtech.com';
+  static const String baseUrl = 'https://carrot.refilex.com';
 
   // https://stackfood-admin.6amtech.com
   static const String configUri = '/api/v1/config';
